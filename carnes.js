8f95a5a4a6c6504fb8ca4294bbda28955f67998b
samantha oreyano <samyoreyano@gmail.com>
2022-10-04 21:42:15 -0300

    cambio hambur

--- carnes.js
+++ carnes.js
@@ -1,26 +1,15 @@
-//  seleccionamos los dos elementos que serán clickables
 
-const toggleButton = document.getElementById("button-menu");
-const navWrapper = document.getElementById("nav");
 
-/* 
-  cada ves que se haga click en el botón 
-  agrega y quita las clases necesarias 
-  para que el menú se muestre.
-*/
-toggleButton.addEventListener("click", () => {
-  toggleButton.classList.toggle("close");
-  navWrapper.classList.toggle("show");
-});
+/* ---- menu hamburguesa ---- */
+// selector
+var menu = document.querySelector('.hamburger');
 
-/* 
-  Cuándo se haga click fuera del contenedor de enlaces 
-  el menú debe esconderse.
-*/
+// metodo
+function toggleMenu (event) {
+	this.classList.toggle('is-active');
+	document.querySelector( ".menuppal" ).classList.toggle("is_active");
+	event.preventDefault();
+}
 
-navWrapper.addEventListener("click", e => {
-  if (e.target.id === "nav") {
-    navWrapper.classList.remove("show");
-    toggleButton.classList.remove("close");
-  }
-});
+// evento
+menu.addEventListener('click', toggleMenu, false);
